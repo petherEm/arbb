ea64f94b5dabb52bd127f07d1161bc06d9336387
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-12-08 16:10:42 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14">
+      <div className="space-y-4 text-xs text-gray-300">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How it works?</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>ARBB Plus</p>
+        <p>ARBB Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-300">
+        <h5 className="font-bold">HOST</h5>
+        <p>Hello</p>
+        <p>Gifts</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-300">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-300">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
