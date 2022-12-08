9455e2a29bc637dd721bd8d61e2c517537a07541
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-12-08 17:24:57 +0100

    new commit

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Banner = () => {
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image src="/bg-hero3.jpg" layout="fill" objectFit="cover" />
+      <Image src="/bg-hero3.jpg" layout="fill" objectFit="cover" alt="banner" />
 
       <div className="absolute top-1/2 w-full text-center text-white ">
         <p className="font-bold text-2xl">Not sure where to go? Perfect.</p>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import {
   SearchIcon,
@@ -6,19 +6,49 @@ import {
   GlobeAltIcon,
   UsersCircleIcon,
   UserCircleIcon,
+  UsersIcon,
 } from "@heroicons/react/solid";
 
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
 const Header = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  }
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-black text-white shadow-md p-5 md:px-10">
       {/* LEFT */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
-        <h1 className="font-bold text-2xl text-rose-400">Ar<span className="text-4xl text-indigo-400">.</span>BB</h1>
+        <h1 className="font-bold text-2xl text-rose-400">
+          Ar<span className="text-4xl text-indigo-400">.</span>BB
+        </h1>
       </div>
 
       {/* MIDDLE */}
       <div className="flex items-center border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none"
           type="text"
           placeholder="Start your search"
@@ -36,6 +66,36 @@ const Header = () => {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto bg-white text-black rounded-lg">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UsersIcon className="h-5" />
+            <input
+              type="number"
+              min={1}
+              value={noOfGuests}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              className="bg-white w-12 pl-2 text-lg outline-none text-red-400"
+            />
+          </div>
+         
+          <div className="flex">
+            <button className="flex-grow text-gray-500">Cancel</button>
+            <button className="flex-grow text-red-500">Search</button>
+          
+          </div>
+        </div>
+      )}
     </header>
   );
 };
